f5ecda94bf0bae0b307cbcd5a467f2d09128dff0
PovilasU <povilas.urbonas85@gmail.com>
2024-03-07 21:17:25 +0000

    updated text

--- povilaswebproject.client/src/App.tsx
+++ povilaswebproject.client/src/App.tsx
@@ -1,56 +1,84 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
+// interface Forecast {
+//     date: string;
+//     temperatureC: number;
+//     temperatureF: number;
+//     summary: string;
+// }
 interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
+  id: number;
+  name: string;
+  price: number;
 }
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+  const [forecasts, setForecasts] = useState<Forecast[]>();
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
+  useEffect(() => {
+    populateWeatherData();
+  }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
+  const contents =
+    forecasts === undefined ? (
+      <p>
+        <em>
+          Loading... Please refresh once the ASP.NET backend has started. See{" "}
+          <a href="https://aka.ms/jspsintegrationreact">
+            https://aka.ms/jspsintegrationreact
+          </a>{" "}
+          for more details.
+        </em>
+      </p>
+    ) : (
+      <table className="table table-striped" aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Price</th>
+            {/* <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th> */}
+            {/* <th>Summary</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map((forecast) => (
+            // <tr key={forecast.date}>
+            <tr key={forecast.id}>
+              {/* <td>{forecast.date}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td> */}
+              <td>{forecast.id}</td>
+              <td>{forecast.name}</td>
+              <td>{forecast.price}</td>
+              {/* <td>{forecast.summary}</td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
 
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+  return (
+    <div>
+      {/* <h1 id="tabelLabel">Weather forecast</h1> */}
+      <h1 id="tabelLabel">Products</h1>
+      <p>This component demonstrates fetching data from the server.</p>
+      {contents}
+    </div>
+  );
+
+  async function populateWeatherData() {
+    // const response = await fetch("weatherforecast");
+    const response = await fetch("products");
+    const data = await response.json();
+    setForecasts(data);
+
+    console.log(data);
+  }
 }
 
 export default App;
